1757813bdf9e1943b66b803f9ec5413d3544a65f
Aldin Nezirić <aldinezi@gmail.com>
2024-07-16 14:25:21 +0200

    add language files

---
+++ src/bs/index.ts
@@ -0,0 +1,43 @@
+import { Translations } from "../types";
+
+const bs = {
+  posted: "Napisao",
+  lastReply: "Zadnji",
+  lastModified: "Zadnja izmjena",
+  loggedOut: "Odjavljen",
+  search: "Pretraga...",
+  close: "Zatvori",
+  all: "svi",
+  send: "Pošalji",
+  resendInviteLink: "Pošalji ponovo link",
+  addComment: "Dodaj komentar",
+  comment: "komentariši",
+  writeComment: "Napiši comment...",
+  comments: "KOMENTARI",
+  createBoard: "Kreiraj Kanban Tablu",
+  createPage: "Kreiraj Stranicu",
+  delete: "Izbriši",
+  edit: "Uredi",
+  remove: "Izbriši",
+  done: "Gotovo",
+  joinSpaceLbl: "Pridruži se prostoru",
+  publish: "Objavi",
+  billing: {
+    ui: {
+      title: "Račun",
+      subtitle: "Pregledajte i upravljajte svojim računom.",
+      plan: "Plan",
+      planDetails: "Detalji plana",
+      paymentMethod: "Način plaćanja",
+      paymentMethodDetails: "Detalji načina plaćanja",
+      invoices: "Fakture",
+      invoicesDetails: "Detalji faktura",
+      subscription: "Pretplata",
+      subscriptionDetails: "Detalji pretplate",
+      usage: "Korištenje",
+      usageDetails: "Detalji korištenja",
+    },
+  },
+};
+
+export default bs;
