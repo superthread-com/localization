5b68e5c6002395d43fd745a48e2b871862cc110e
Aldin Nezirić <aldinezi@gmail.com>
2024-07-16 17:06:47 +0200

    superthread localization repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# localization
+Superthread front-end localizations
---
+++ src/fr/index.ts
@@ -0,0 +1,23 @@
+export default {
+  // onboarding
+  switchAccount: "Changer de compte",
+  inviteTeamMembersTo:
+    "Inviter des membres de votre équipe a votre espace de travail",
+  loggingIn: "En train de vous connecter",
+  defaultSpaceName: "Général",
+  onboardingSpaceName: "Bienvenue 👋",
+  loginCodeSendAgain: "Remettez votre code d'accès",
+  loginCodeResent: "Votre code d'accès a été renvoyé.",
+  loginCodeError: "Mauvais code de verification.",
+  loginCodeErrorSending:
+    "Il y a eu un problem avec l'envoie de votre code d'accès.",
+  loginCodeErrorExpired:
+    "Votre code d'accès n'est plus valide, nous vous avons envoyé un autre.",
+  loginCodeErrorRateLimited:
+    "Vous avez essayé trop de fois, veuyez réessayer plus tard.",
+  all: "Tout",
+  posted: "Affiché",
+  lastReply: "Dernière réponse",
+  lastModified: "Modifié",
+  loggedOut: "Déconnecté",
+};
