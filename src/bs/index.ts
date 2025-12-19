927ec0485edc5cf7bd70ec67414a9045ccd91809
Ali Halilovic <ali@superthread.com>
2025-12-19 09:39:59 +0100

    Fix bosnian

--- src/bs/index.ts
+++ src/bs/index.ts
@@ -2960,12 +2960,12 @@ const bs: Translations = {
     'Napravite Karticu nazvanu "Nova Kartica" unutar vaše Nove Tablice Klijenta',
   move_card_between_spaces:
     "Prevucite svoju novu karticu od Novi Klijent do Novi Odjel",
-  moveBoard: "Premjesti tablu",
+  moveBoard: "Premjesti ploču",
   moveBoardDescription:
-    "Neki tagovi korišteni u ovoj tabli ne postoje u %{space} prostoru i bit će uklonjeni sa kartica.",
-  missingTags: "Tagovi koji nedostaju",
+    "Neke oznake korištene u ovoj ploči ne postoje u %{space} prostoru i bit će uklonjeni sa kartica.",
+  missingTags: "Oznake koje nedostaju",
   createMissingTagsInSpaceDescription:
-    "Kreirajte tagove u %{space} prostoru ako ne postoje",
+    "Kreirajte oznake u %{space} prostoru ako ne postoje",
 };
 
 export default bs;
